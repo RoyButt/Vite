4a850f279d18073abf60b5f29827925eebb79e9d
Rehan Butt <55927635+CourseMaterial@users.noreply.github.com>
2023-11-02 10:29:00 +0500

    Your commit message e

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Specify your custom host
-    port: 3001, // Specify your desired port
+    port: 3000, // Specify your desired port
   },
 })
